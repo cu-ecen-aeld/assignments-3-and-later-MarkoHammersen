5c20534def2b0c1abb953689d34397e1c29364d3
Marko Hammersen <marko.hammersen@gmail.com>
2025-11-13 19:21:30 +0100

    not yet ready

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -2,8 +2,8 @@
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
 
-set -e
-set -u
+set -e # Exit on error
+set -u # Treat unset variables as an error
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
@@ -13,6 +13,7 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
+# if the number of arguments is less than 1 (i.e., none were passed).
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -21,7 +22,11 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
+# create output directory if it does not exist
 mkdir -p ${OUTDIR}
+
+export PATH=/bin:/usr/bin:/usr/local/bin:/opt/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin:$PATH
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
@@ -29,12 +34,19 @@ if [ ! -d "${OUTDIR}/linux-stable" ]; then
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; # build kernel only if doesn't exit yet.
+then 
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # TODO: Add your kernel build steps here - 1.c
+    echo "Building the Linux Kernel"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper #deep clean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig #default config the board
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all #build a kernel image
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules #build kernel modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs #build device tree blobs
 fi
 
 echo "Adding the Image in outdir"
@@ -44,18 +56,28 @@ cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    rm -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
+
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    #git clone git://busybox.net/busybox.git
+    git clone https://github.com/mirror/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install 
 else
     cd busybox
 fi
@@ -63,10 +85,15 @@ fi
 # TODO: Make and install busybox
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# # TODO: Add library dependencies to rootfs
+# SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+# LIBC=$(${CROSS_COMPILE}gcc -print-file-name=libc.so).6
+# LIBM=$(${CROSS_COMPILE}gcc -print-file-name=libm.so).6
+# LIBDL=$(${CROSS_COMPILE}gcc -print-file-name=libdl.so).2
+
 
 # TODO: Make device nodes
 
