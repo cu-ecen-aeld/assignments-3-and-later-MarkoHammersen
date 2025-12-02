038750403dff617c2b5cafbde7f2b188498d44de
MarkoHammersen <marko.hammersen@gmail.com>
2025-12-02 18:10:50 +0100

    assignment 9

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -21,6 +21,7 @@
 #include <asm/uaccess.h>
 #include "aesd-circular-buffer.h"
 #include "aesdchar.h"
+#include "aesd_ioctl.h"
 
 static int aesd_major = 0; // use dynamic major
 static int aesd_minor = 0;
@@ -168,12 +169,137 @@ static ssize_t aesd_write(struct file *filp, const char __user *buf, size_t coun
     return retval;
 }
 
+static loff_t aesd_llseek(struct file *filp, loff_t off, int whence)
+{
+    struct aesd_dev* dev = filp->private_data;
+    loff_t pos;
+
+    if (mutex_lock_interruptible(&dev->lock))
+    {
+        PDEBUG("failed acquire lock");
+        return -ERESTARTSYS;
+    }  
+
+    // get total size
+    uint32_t i;
+    loff_t total_size;
+    struct aesd_circular_buffer *cbuf = &dev->cbuf;
+
+    if(cbuf->full)
+    {
+        total_size = dev->cbuf.entry[cbuf->in_offs].size;
+        i = (cbuf->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+    else
+    {
+        total_size = 0;
+        i = cbuf->in_offs;
+    }
+
+    for(i = i; i != cbuf->out_offs; i = (i + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+    {
+        total_size += dev->cbuf.entry[i].size;
+    }
+
+    switch (whence)
+    {
+    case SEEK_SET:
+        pos = off;
+        break;
+
+    case SEEK_CUR:
+        pos = filp->f_pos + off;
+        break;
+
+    case SEEK_END:
+        pos = total_size + off;
+        break;
+
+    default:
+        mutex_unlock(&dev->lock);
+        return -EINVAL;
+    }
+
+    if(pos < 0)
+    {
+        mutex_unlock(&dev->lock);
+        return -EINVAL;
+    }
+
+    filp->f_pos = pos;
+    mutex_unlock(&dev->lock);
+
+    return pos;
+}
+
+static long aesd_seekto(struct file *filp, struct aesd_seekto *params)
+{
+    uint32_t i;
+    long total_size;
+    uint32_t index_write_cmd;
+    struct aesd_dev* dev = filp->private_data;
+    struct aesd_circular_buffer *cbuf = &dev->cbuf;
+    long ret;
+
+    if (mutex_lock_interruptible(&dev->lock))
+    {
+        PDEBUG("failed acquire lock");
+        return -ERESTARTSYS;
+    }  
+
+
+    index_write_cmd = 0;
+    i = cbuf->in_offs;
+    total_size = 0;
+
+    bool found_it = false;
+    while((i != cbuf->out_offs) || (index_write_cmd == 0 && cbuf->full))
+    {
+        if(index_write_cmd == params->write_cmd)
+        {
+            found_it = true;
+            break;
+        }
+        total_size += dev->cbuf.entry[i].size;
+        i = (i + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    if(!found_it)
+    {
+        mutex_unlock(&dev->lock);
+        return EINVAL;
+    }
+
+    ret = total_size + (long)params->write_cmd_offset;
+    filp->f_pos = ret;
+    mutex_unlock(&dev->lock);
+
+    return ret;
+}
+
+static long aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+    if(AESDCHAR_IOCSEEKTO == cmd)
+    {
+        struct aesd_seekto params;
+        if(copy_from_user(&params, (const void __user *)arg, sizeof(struct aesd_seekto)))
+        {
+            return EFAULT;
+        }
+        return aesd_seekto(filp, &params);
+    }
+
+    return EINVAL;
+}
+
 struct file_operations aesd_fops = {
     .owner = THIS_MODULE,
     .read = aesd_read,
     .write = aesd_write,
     .open = aesd_open,
     .release = aesd_release,
+    .llseek = aesd_llseek,
+    .unlocked_ioctl = aesd_ioctl
 };
 
 static int aesd_setup_cdev(struct aesd_dev * dev)
@@ -223,16 +349,15 @@ static int aesd_init_module(void)
 static void aesd_cleanup_module(void)
 {
     dev_t devno = MKDEV(aesd_major, aesd_minor);
-
     cdev_del(&aesd_device.cdev);
 
     /**
      * TODO: cleanup AESD specific poritions here as necessary
      */
     uint32_t index;
-    struct aesd_circular_buffer buffer;
+    struct aesd_circular_buffer *buffer = &aesd_device.cbuf;
     struct aesd_buffer_entry *entry;
-    AESD_CIRCULAR_BUFFER_FOREACH(entry,&buffer,index) 
+    AESD_CIRCULAR_BUFFER_FOREACH(entry,buffer,index) 
     {
         kfree(entry->buffptr);
     }    
